6eab264db52060217027f11e7fa03d801a6e290d
Deniz Boz <boz.dnz@gmail.com>
2023-07-13 13:48:14 +0300

    Distribution save/load system and editor tester. Binary save/load not yet tested.

--- Assets/Scripts/Managers/SlotMachine.cs
+++ Assets/Scripts/Managers/SlotMachine.cs
@@ -23,24 +23,32 @@ namespace Managers
         {
             if (!PlayerPrefs.HasKey(keyForCurrentRound))
                 PlayerPrefs.SetInt(keyForCurrentRound, 0);
+
+            m_currentRound = PlayerPrefs.GetInt(keyForCurrentRound);
+            m_totalRoundCount = m_probDistribution.GetTotalOccurenceCount();
+
+            m_currentRound %= m_totalRoundCount;
+            
+            if (m_currentRound == 0)
+            {
+                var newDistribution = CreateNewDistribution();
+            }
         }
 
+        private Lineup[] CreateNewDistribution()
+        {
+            var lineupOccurrences = m_probDistribution.LineupOccurrences;
+            var generator = new DistributionGenerator<Lineup>(lineupOccurrences);
+
+            return generator.GetDistribution();
+        }
+        
         protected override void Resolve()
         {
         }
 
         protected override void OnStart()
         {
-            // debug purpose
-            
-            var generator = new DistributionGenerator<Lineup>(m_probDistribution.LineupOccurrences);
-            var dist = generator.GetDistribution();
-            
-            for (var i = 0; i < dist.Length; i++)
-            {
-                var lineup = dist[i];
-                Debug.Log($"{i}: {lineup.Left} | {lineup.Middle} | {lineup.Right}");
-            }
         }
     }
 }
---
+++ Assets/Scripts/Utility/DataSystem.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+namespace Utility
+{
+    public static class DataSystem
+    {
+        private const string binaryPath = "SavedData/data.bin";
+
+
+        public static void SaveBinary<T>(T[] items)
+        {
+            var path = Path.Combine(Application.persistentDataPath, binaryPath);
+            
+            var fileMode = File.Exists(path) ? FileMode.Open : FileMode.Create;
+            var stream = new FileStream(path, fileMode);
+            
+            BinaryFormatter formatter = new BinaryFormatter();
+            
+            formatter.Serialize(stream, items);
+            stream.Close();
+        }
+        
+        public static T[] LoadBinary<T>()
+        {
+            var path = Path.Combine(Application.persistentDataPath, binaryPath);
+
+            if (!File.Exists(path))
+                throw new Exception("Ensure the data saved first.");
+            
+            var stream = new FileStream(path, FileMode.Open);
+
+            BinaryFormatter formatter = new BinaryFormatter();
+            var items = formatter.Deserialize(stream) as T[];
+            
+            stream.Close();
+            return items;
+        }
+        
+        /// <summary>
+        /// For debug purposes only.
+        /// </summary>
+        public static void SaveJson<T>(T[] items, string path)
+        {
+            var fileMode = File.Exists(path) ? FileMode.Open : FileMode.Create;
+            
+            var stream = new FileStream(path, fileMode);
+            var writer = new StreamWriter(stream);
+            
+            for (var i = 0; i < items.Length; i++)
+            {
+                var line = JsonUtility.ToJson(items[i]);
+                writer.WriteLine(line);
+            }
+            
+            writer.Close();
+            stream.Close();
+        }
+    }
+}
